5dfa0dd27c08a1f84bc433bcfc80744f984d38ad
Vanessa Victorino <vanessaf.victorino@yahoo.com>
2026-04-09 10:35:37 -0700

    refactor: remove duplicate template route and use admin route

--- server/src/routes/template.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getTemplates } from "../controllers/template.controller";
-
-const router = Router();
-
-router.get("/", getTemplates);
-
-export default router;
